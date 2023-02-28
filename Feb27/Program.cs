d35801aa7ba379e17bb08ace5b977686617b58c7
szhang53 <junyuanterem@gmail.com>
2023-02-27 18:03:47 -0700

    Creat method

--- Feb27/Program.cs
+++ Feb27/Program.cs
@@ -1,8 +1,14 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 class ClassProgramming
 {
+    public static void Display()
+    {
+        Console.WriteLine("Use of methods");
+    }
     public static void Main(string[] args)
     {
-
+        Display();
     }
 }
